ab843517c915742149c786de5dca6b9ab87f9ecc
elnur549 <elnurtaken@gmail.com>
2026-08-12 02:43:21 +0400

    feat: add Orders Filament resource with Items relation manager

---
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Resources\Orders\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class OrdersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('Order ID')
+                    ->searchable()
+                    ->copyable(),
+
+                TextColumn::make('status')
+                    ->badge()
+                    ->sortable(),
+
+                TextColumn::make('total')
+                    ->money('USD')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
