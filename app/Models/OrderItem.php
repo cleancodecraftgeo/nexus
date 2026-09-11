d061267db7211c0661ddf5531fbfcb3e9203efaa
elnur549 <elnurtaken@gmail.com>
2026-09-12 01:35:59 +0400

    add product variant relation to order items

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -14,6 +14,7 @@ class OrderItem extends Model
     protected $fillable = [
         'order_id',
         'product_id',
+        'product_variant_id',
         'quantity',
         'price',
     ];
@@ -26,5 +27,9 @@ class OrderItem extends Model
     public function product():BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+    public function variant():BelongsTo
+    {
+        return $this->belongsTo(ProductVariant::class,'product_variant_id');
     }
 }
---
+++ database/migrations/2026_09_10_195806_add_product_variant_id_to_order_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->foreignUlid('product_variant_id')
+            ->nullable()
+            ->constrained('product_variants')
+            ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+             $table->dropForeign(['product_variant_id']);
+        $table->dropColumn('product_variant_id');
+        });
+    }
+};
