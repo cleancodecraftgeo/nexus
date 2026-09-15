4fc924a8ed90916c129a4692e9e47458ff969879
elnur549 <elnurtaken@gmail.com>
2026-09-15 04:21:05 +0400

    add user roles and filament authorization policies

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use App\Enums\UserRole;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Override;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -21,11 +25,48 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function isSuperAdmin():bool
+    {
+        return $this->role === UserRole::SuperAdmin;
+    }
+
+        public function isAdmin():bool
+    {
+        return $this->role === UserRole::Admin;
+    }
+
+    public function isManager():bool
+    {
+        return $this->role ===UserRole::Manager;
+    }
+
+        public function isCustomer():bool
+    {
+        return $this->role === UserRole::Customer;
+    }
+
+    public function canAccessAdminPanel(): bool
+    {
+        return in_array($this->role,[
+            UserRole::SuperAdmin,
+            UserRole::Admin,
+            UserRole::Manager
+        ],true);
+    }
+
+    
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->canAccessAdminPanel();
+    }
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role'=>UserRole::class
         ];
     }
+
 }
--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -3,30 +3,34 @@
 namespace App\Policies;
 
 use App\Enums\UserRole;
-use App\Models\Product;
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ProductPolicy
+class OrderPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return in_array($user->role, [
-            UserRole::Manager,
+        return in_array($user->role,[
+            UserRole::SuperAdmin,
             UserRole::Admin,
-            UserRole::SuperAdmin
+            UserRole::Manager,
         ],true);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Product $product): bool
+    public function view(User $user, Order $order): bool
     {
-        return true;
+        return in_array($user->role,[
+            UserRole::SuperAdmin,
+            UserRole::Admin,
+            UserRole::Manager
+        ],true);
     }
 
     /**
@@ -34,44 +38,45 @@ class ProductPolicy
      */
     public function create(User $user): bool
     {
-        return in_array($user->role, [
-            UserRole::Admin,
-            UserRole::SuperAdmin,
-        ],true);
+        return true;
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Product $product): bool
+    public function update(User $user, Order $order): bool
     {
         return in_array($user->role,[
-            UserRole::Admin,
             UserRole::SuperAdmin,
+            UserRole::Admin,
+            UserRole::Manager,
         ],true);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Product $product): bool
+    public function delete(User $user, Order $order): bool
     {
-        return $user->role === UserRole::SuperAdmin;
+        return in_array($user->role,[
+            UserRole::SuperAdmin,
+            UserRole::Admin
+        ],true);
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Product $product): bool
+    public function restore(User $user, Order $order): bool
     {
-        return true;
+        return false;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Product $product): bool
+    public function forceDelete(User $user, Order $order): bool
     {
-        return true;
+        return false;
     }
 }
